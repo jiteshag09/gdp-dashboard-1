7e2c7d975c6da3add82fbf75e4cb80427280e295
jiteshag09 <71830464+jiteshag09@users.noreply.github.com>
2025-07-05 13:32:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,186 @@
+"""
+Indices Heatmap Visualizer
+
+-- Dependencies to be installed --
+pip install streamlit
+pip install pandas
+pip install matplotlib
+
+Disclaimer:
+The information provided is for educational and informational purposes only and
+should not be construed as financial, investment, or legal advice. The content is based on publicly available
+information and personal opinions and may not be suitable for all investors. Investing involves risks,
+including the loss of principal.
+
+Queries on feedback on the python screener can be sent to :
+FabTrader (fabtraderinc@gmail.com)
+www.fabtrader.in
+YouTube: @fabtraderinc
+X / Instagram / Telegram :  @Iamfabtrader
+"""
+from logging import exception
+
 import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
+import plotly.express as px
+import requests
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
-
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
+@st.cache_data(ttl=300)
+def get_index_details(category):
+    """
+    Function that returns constituents and price change / mcap data for indices
+    :param category: Index
+    :return: Dataframe containing Price Change and Market Cap data for all index constituents
     """
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
+        'Upgrade-Insecure-Requests': "1",
+        "DNT": "1",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*,q=0.8",
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate',
+        'Connection': 'keep-alive'
+    }
+    category = category.upper().replace('&', '%26').replace(' ', '%20')
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+    try:
+        ref_url = "https://www.nseindia.com/market-data/live-equity-market?symbol={category}"
+        ref = requests.get(ref_url, headers=headers)
+        url = f"https://www.nseindia.com/api/equity-stockIndices?index={category}"
+        data = requests.get(url, headers=headers, cookies=ref.cookies.get_dict()).json()
+        df = pd.DataFrame(data['data'])
+        if not df.empty:
+            df = df.drop(["meta"], axis=1)
+            df = df.set_index("symbol", drop=True)
+            df['ffmc'] = round(df['ffmc']/10000000, 0)
+            df = df.iloc[1:].reset_index(drop=False)
+        return df
+    except Exception as e:
+        print("Error Fetching Index Data from NSE. Aborting....")
+        return pd.DataFrame()
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+# Include any additional NSE indices to list below
+index_list = ['NIFTY 50', 'NIFTY NEXT 50', 'NIFTY MIDCAP 50', 'NIFTY MIDCAP 100', 'NIFTY MIDCAP 150',
+                      'NIFTY SMALLCAP 50',
+                      'NIFTY SMALLCAP 100', 'NIFTY SMALLCAP 250', 'NIFTY MIDSMALLCAP 400', 'NIFTY 100', 'NIFTY 200',
+                      'NIFTY AUTO',
+                      'NIFTY BANK', 'NIFTY ENERGY', 'NIFTY FINANCIAL SERVICES', 'NIFTY FINANCIAL SERVICES 25/50',
+                      'NIFTY FMCG',
+                      'NIFTY IT', 'NIFTY MEDIA', 'NIFTY METAL', 'NIFTY PHARMA', 'NIFTY PSU BANK', 'NIFTY REALTY',
+                      'NIFTY PRIVATE BANK', 'Securities in F&O', 'Permitted to Trade',
+                      'NIFTY DIVIDEND OPPORTUNITIES 50',
+                      'NIFTY50 VALUE 20', 'NIFTY100 QUALITY 30', 'NIFTY50 EQUAL WEIGHT', 'NIFTY100 EQUAL WEIGHT',
+                      'NIFTY100 LOW VOLATILITY 30', 'NIFTY ALPHA 50', 'NIFTY200 QUALITY 30',
+                      'NIFTY ALPHA LOW-VOLATILITY 30',
+                      'NIFTY200 MOMENTUM 30', 'NIFTY COMMODITIES', 'NIFTY INDIA CONSUMPTION', 'NIFTY CPSE',
+                      'NIFTY INFRASTRUCTURE',
+                      'NIFTY MNC', 'NIFTY GROWTH SECTORS 15', 'NIFTY PSE', 'NIFTY SERVICES SECTOR',
+                      'NIFTY100 LIQUID 15',
+                      'NIFTY MIDCAP LIQUID 15']
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
 
-    return gdp_df
+pd.set_option("display.max_rows", None, "display.max_columns", None)
 
-gdp_df = get_gdp_data()
+# Set initial page configuration for app
+st.set_page_config(
+    page_title='FabTrader - Algo Trading : Market Analytics Dashboard',
+    layout="centered")
 
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
+# Apply fixed screen width for app (1440px)
+st.markdown(
+    f"""
+    <style>
+      .stAppViewContainer .stMain .stMainBlockContainer{{ max-width: 1440px; }}
+    </style>    
+  """,
+    unsafe_allow_html=True,
 )
 
-''
-''
+# Streamlit App
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+header1, header2 = st.columns([3,1])
+with header1:
+# with st.container():
+    st.image("https://fabtrader.in/wp-content/uploads/2025/05/appLogo.png")
+    st.subheader("NSE Indices Heatmap - Visualizer")
+    col1, col2, _ = st.columns([2,1,1])
+    index_filter = col1.selectbox("Choose Index", index_list, index=0)
+    slice_by = col2.selectbox("Slice By", ["Market Cap","Gainers","Losers"], index=0)
 
-st.header(f'GDP in {to_year}', divider='gray')
+with header2:
+    df = get_index_details(index_filter)
+    advances = df[df['pChange'] > 0].shape[0]
+    declines = df[df['pChange'] < 0].shape[0]
+    no_change = df[df['pChange'] == 0].shape[0]
+    total_count = advances + declines + no_change
 
-''
+    # Plot pie chart
 
-cols = st.columns(4)
+    fig = px.pie(names=['Advances','Declines','No Change'],
+                 values=[advances, declines, no_change],
+                 color=['Advances','Declines','No Change'],
+                 # color_discrete_sequence=['#2ecc71', '#e74c3c', '#95a5a6'])
+                 color_discrete_sequence=['#3AA864', '#F38039', '#F2F2F2'])
+    fig.update_traces(hole=0.7)
+    fig.update_traces(textinfo='none')
+    fig.update_layout(
+        width=200,  # width in pixels
+        height=200,  # height in pixels
+        showlegend=False,
+        annotations=[dict(
+            text=f'{advances}<br>Advances<br>{declines}<br>Declines',  # Line break for style
+            x=0.5, y=0.5, font_size=14, showarrow=False
+        )]
+    )
+    fig.update_layout(
+        margin=dict(l=0, r=0, t=0, b=0)  # left, right, top, bottom
+    )
+    st.plotly_chart(fig)
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
 
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+if not df.empty:
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
+    if slice_by == 'Market Cap':
+        slice_factor = 'ffmc'
+        color_scale = ['#ff7a3a', 'white', 'green']
+    elif slice_by == 'Gainers':
+        slice_factor = 'pChange'
+        color_scale = ['white', '#a5eb79']
+    elif slice_by == 'Losers':
+        df = df[df["pChange"] < 0]
+        df['Abs'] = df['pChange'].abs()
+        slice_factor = 'Abs'
+        color_scale = ['#ff7a3a', 'white']
 
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
+    # Plotly Treemap
+    st.divider()
+    fig = px.treemap(
+        df,
+        path=['symbol'],
+        values=slice_factor,
+        color='pChange',
+        color_continuous_scale=color_scale,
+        custom_data=['pChange']
+    )
+    fig.update_layout(
+        margin=dict(t=30, l=0, r=0, b=0),
+        width=500,height=1000,
+        paper_bgcolor="rgba(0, 0, 0, 0)", plot_bgcolor="rgba(0, 0, 0, 0)",
         )
+
+    fig.update_traces(
+        hovertemplate='<b>%{label}</b><br>Size: %{value}<br>pChange: %{customdata[0]:.2f}%',
+        texttemplate='%{label}<br>%{customdata[0]:.2f}%',
+        textposition='middle center'
+    )
+    fig.update_traces(textinfo="label+value")
+    fig.update_coloraxes(showscale=False)
+    st.plotly_chart(fig, use_container_width=True)
+
+else:
+    st.warning("Failed to fetch data.")
+st.write("")
+st.write(":gray[Made with :heart: by FabTrader. ©2024 Fabtrader.in - All Rights Reserved]")
